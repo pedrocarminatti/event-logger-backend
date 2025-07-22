1979b381790de20d6bc7c491a4f58ae7cd5777e3
Pedro Carminatti <pedrolrc13@gmail.com>
2025-07-22 15:57:06 -0300

    feat: initialize Spring Boot project with Java 21 and base folder structure

---
+++ src/test/java/com/el/event_logger/EventLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.el.event_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
